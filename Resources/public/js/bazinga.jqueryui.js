9dab4ec5396603d3666fe79bd473c2a531795c77
William DURAND <william.durand1@gmail.com>
2011-05-06 16:18:26 +0200

    Added js

---
+++ Resources/public/js/bazinga.jqueryui.js
@@ -0,0 +1,25 @@
+(function($, undefined){
+    $(document).ready(function() {
+        $('.ui-button').each(function() {
+            $(this).hover(function() {
+                $(this).addClass('ui-state-hover');
+            }, function() {
+                $(this).removeClass('ui-state-hover');
+            });
+            $(this).focus(function() {
+                $(this).addClass('ui-state-focus');
+            }, function() {
+                $(this).removeClass('ui-state-focus');
+            });
+            $(this).mousedown(function() {
+                $(this).addClass('ui-state-active');
+            });
+            $(this).mouseup(function() {
+                $(this).removeClass('ui-state-active');
+            });
+            $(this).mouseleave(function() {
+                $(this).removeClass('ui-state-active');
+            });
+        });
+    });
+})(jQuery);
