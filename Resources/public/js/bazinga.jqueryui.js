763babbc5f4469b9449810bee7130c7b94fde365
William DURAND <william.durand1@gmail.com>
2011-05-18 18:14:25 +0200

    Fixed JS

--- Resources/public/js/bazinga.jqueryui.js
+++ Resources/public/js/bazinga.jqueryui.js
@@ -1,29 +1,29 @@
-(function($, undefined){
-    $(document).ready(function() {
-        $('.ui-button').each(function() {
-            $(this).hover(function() {
-                $(this).addClass('ui-state-hover');
-            }, function() {
-                $(this).removeClass('ui-state-hover');
-            });
-            $(this).focus(function() {
-                $(this).addClass('ui-state-focus');
-            }, function() {
-                $(this).removeClass('ui-state-focus');
-            });
-            $(this).mousedown(function() {
-                $(this).addClass('ui-state-active');
-            });
-            $(this).mouseup(function() {
-                $(this).removeClass('ui-state-active');
-            });
-            $(this).mouseleave(function() {
-                $(this).removeClass('ui-state-active');
-            });
-        });
-
-        if ($.uniform) {
-            $("select, input:checkbox, input:radio, input:file, input:text, input:password").uniform();
-        }
+(function($, undefined) {
+  $(document).ready(function() {
+    $('.ui-button').each(function() {
+      $(this).hover(function() {
+        $(this).addClass('ui-state-hover');
+      }, function() {
+        $(this).removeClass('ui-state-hover');
+      });
+      $(this).focus(function() {
+        $(this).addClass('ui-state-focus');
+      }, function() {
+        $(this).removeClass('ui-state-focus');
+      });
+      $(this).mousedown(function() {
+        $(this).addClass('ui-state-active');
+      });
+      $(this).mouseup(function() {
+        $(this).removeClass('ui-state-active');
+      });
+      $(this).mouseleave(function() {
+        $(this).removeClass('ui-state-active');
+      });
     });
+
+    if ($.uniform) {
+      $('.uniformize').uniform();
+    }
+  });
 })(jQuery);
