69ab4c10d4aa4dcfc5ced12c8f8dea343597fea0
William DURAND <william.durand1@gmail.com>
2011-11-14 18:17:01 +0100

    Added a way to launch unit tests in the bundle itself

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,18 +1,28 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+$vendorDir = __DIR__.'/../vendor';
+require_once $vendorDir.'/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
+$loader->registerNamespaces(array(
+    'Symfony'           => array($vendorDir.'/symfony/src'),
+    'Faker'             => array($vendorDir.'/twig/src'),
+));
+$loader->registerPrefixes(array(
+    'Twig_Extensions_' => $vendorDir.'/twig-extensions/lib',
+    'Twig_'            => $vendorDir.'/twig/lib',
+));
 $loader->register();
 
-spl_autoload_register(function($class)
-{
+spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Bazinga\\JqueryUiBundle\\')) {
-        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        require_once __DIR__.'/../'.$path;
+        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        if (!stream_resolve_include_path($path)) {
+            return false;
+        }
+        require_once $path;
         return true;
     }
 });
